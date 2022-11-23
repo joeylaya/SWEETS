f6aee2b268d12600139abc5a9cacbe3453332ca2
Joey Laya <joeyaztan@gmail.com>
2022-11-22 21:04:22 -0800

    global, dataStructure, and python apis

--- stores/pythonStore.ts
+++ stores/pythonStore.ts
@@ -1,9 +1,36 @@
+import {
+  DataType,
+  DataType_Method,
+  Method
+} from "~~/types";
+
 export const usePythonStore = defineStore('python', {
   state: () => ({
-
+    dataTypes: Array<Object>() as Array<DataType>,
+    dataType_Methods: Array<Object>() as Array<DataType_Method>,
+    methods: Array<Object>() as Array<Method>,
   }),
 
   actions: {
-    
+    async getAllDataTypes() {
+      if (this.dataTypes.length > 0) return this.dataTypes
+      const { data } = await useFetch('/api/DataType')
+      this.dataTypes = data.value as Array<DataType>
+      return this.dataTypes
+    },
+
+    async getAllDataType_Methods() {
+      if (this.dataType_Methods.length > 0) return this.dataType_Methods
+      const { data } = await useFetch('/api/DataType_Method')
+      this.dataType_Methods = data.value as Array<DataType_Method>
+      return this.dataType_Methods
+    },
+
+    async getAllMethods() {
+      if (this.methods.length > 0) return this.methods
+      const { data } = await useFetch('/api/Method')
+      this.methods = data.value as Array<Method>
+      return this.methods
+    },    
   }
 })
