ac32a8ee53315064c7ebbd65e9e6f6fdedaca712
Joey Laya <joeyaztan@gmail.com>
2022-11-23 00:00:48 -0800

    landing; supabase connection fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import { useGlobalStore } from '~~/stores/globalStore';
 import { storeToRefs } from 'pinia';
 
@@ -6,16 +6,18 @@ const config = useRuntimeConfig()
 const storageUrl = config.storageUrl
 
 const globalStore = useGlobalStore()
-const { darkMode } = storeToRefs(globalStore)
 
+const { darkMode } = storeToRefs(globalStore)
 const mode = computed(() => {
   if (darkMode.value) return "Dark"
   return "Light"
 })
+
+const topics = await globalStore.getAllTopics()
 </script>
 
 <template>
-  <section>
+  <section class="min-w-screen min-h-screen flex flex-col gap-8 justify-center place-items-center">
     <header class="flex flex-col place-items-center">
         <img
           :src="`${storageUrl}/imgs/sweetsHorizontal${mode}.png`"
@@ -24,9 +26,19 @@ const mode = computed(() => {
         >
         <h2 class="header-2 font-primary">Software Engineer Energy & Time Saver</h2>
       </header>
-      <div class="flex flex-wrap">
-        <div v-for="topic in topics">
-
+      <div class="flex flex-wrap gap-4 justify-center bg-gradient-4 bg-clip-text">
+        <div v-for="topic in topics" :key="topic.id">
+          <NuxtLink :to="`/${topic.slug}`">
+            <button
+              class="
+                font-header font-semibold text-lg sm:(text-xl)
+                bg-transparent text-transparent
+                px-4 py-2 rounded-xl
+              "
+            >
+              {{ topic.name }}
+            </button>
+          </NuxtLink>
         </div>
         <!-- v-for Topic -->
       </div>
